d40f34dadebff88155eb13917b20a6dadf4f9c92
Alev Sametoglu <alevsametoglu.fr@gmail.com>
2023-06-08 11:59:27 +0200

    add steps to test

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,46 +1,55 @@
-const { test, expect } = require("@playwright/test");
-const LoginPage = require("../Pages/LoginPage");
-
+import { test, expect } from "../core/myTest";
 
 test.describe("Login functionality", () => {
-  let loginPage;
-
-  test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
-  });
   // Test case for successful login with valid credentials
-  test("should log in successfully with valid credentials", async ({ page }) => {
-    await loginPage.loginWithValidelCredentials();
+  test("should log in successfully with valid credentials", async ({ page, loginPage }) => {
+    await test.step("Perform succesful log in ", async () => {
+      await loginPage.loginWithValidelCredentials();
+    });
     // Check if the user link is visible, indicating successful login
-    const userLink = page.getByTestId("header-user-links-toggle");
-    await expect(userLink).toBeVisible();
+    await test.step("Check if the user link is visible", async () => {
+      const userLink = page.getByTestId("header-user-links-toggle");
+      await expect(userLink).toBeVisible();
+    });
   });
 
   // Test case for unsuccessful login with invalid credentials
-  test("should not log in with invalid credentials", async ({ page }) => {
+  test("should not log in with invalid credentials", async ({ page, loginPage }) => {
     // Call the login method with invalid email and password
-    await loginPage.loginWithInvalideCredentials();
+    await test.step("Perform login with invalid credentials", async () => {
+      await loginPage.loginWithValidelCredentials();
+    });
 
     // Check if the error message is visible, indicating unsuccessful login
-    const errorMessage = page.getByText("Email ou mot de passe incorrect");
-    await expect(errorMessage).toBeVisible();
+    await test.step("Check if error message is visible", async () => {
+      const errorMessage = page.getByTestId("modal-content-home").locator("div").filter({ hasText: "Email ou mot de passe incorrect" }).nth(1);
+      await expect(errorMessage).toBeTruthy();
+    });
   });
 
-  test("should not login with invalid email format", async ({ page }) => {
+  test("should not login with invalid email format", async ({ page, loginPage }) => {
     // Call the login method with invalid email format
-    await loginPage.loginWithInvalidMail();
+    await test.step("Perform login with invalid email", async () => {
+      await loginPage.loginWithInvalidMail();
+    });
 
-    // Check if the error message is visible, indicating unsuccessful login
-    const invalidMailError = page.locator("div").filter({ hasText: /^Email invalide$/ });
-    await expect(invalidMailError).toBeTruthy();
+    // Check if email validation error message is visible, indicating unsuccessful login
+    await test.step("Check if email validation error message is visible", async () => {
+      const emailError = page.locator("div").filter({ hasText: /^Email invalide$/ });
+      await expect(emailError).toHaveText("Email invalide");
+    });
   });
 
-  test("should not login with invalid password ", async ({ page }) => {
+  test("should not login with invalid password ", async ({ page, loginPage }) => {
     // Call the login method with invalid password
-    await loginPage.loginWithInvalidPassword();
+    await test.step("Perform login with invalid password", async () => {
+      await loginPage.loginWithInvalidPassword();
+    });
 
     // Check if the error message is visible, indicating unsuccessful login
-    const invalidPasswordError = page.getByText("Doit contenir au minimum 8 caractères");
-    await expect(invalidPasswordError).toBeTruthy();
+    await test.step("Check if password validation error message is visible", async () => {
+      const passwordError = page.locator("div").filter({ hasText: /^Doit contenir au minimum 8 caractères$/ });
+      await expect(passwordError).toHaveText("Doit contenir au minimum 8 caractères");
+    });
   });
 });
